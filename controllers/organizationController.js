8b393dab3b59631e601b04adb4ba75ec48b323ba
Jaya Lakshmi V <vjaya@itideology.net>
2019-09-11 19:03:09 +0530

    new updates

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -3,22 +3,43 @@ let orgController = {}
 
 orgController.addOrganization = async (req, res) => {
     // console.log("body", req.body)
-    try {
-        let orgData = new orgModel(req.body);
-        await orgData.save();
-        let output = {
-            msg: "Organization added successfully.",
-            condition: true
-        };
-        res.send(output)
-    }
-    catch (error) {
-        let output = {
-            msg: "Error in adding organization.",
-            condition: false
+
+    if(req.body._id) {
+        try {
+            await orgModel.updateOne({ _id : req.body._id }, { $set : req.body })
+            let output = {
+                msg: "Organization data updated successfully.",
+                condition: true
+            };
+            res.send(output)
         }
-        res.send(output)
+        catch (error) {
+            let output = {
+                msg: "Error in updating organization data.",
+                condition: false
+            }
+            res.send(output)
+        }
     }
+    else {
+        try {
+            let orgData = new orgModel(req.body);
+            await orgData.save();
+            let output = {
+                msg: "Organization data added successfully.",
+                condition: true
+            };
+            res.send(output)
+        }
+        catch (error) {
+            let output = {
+                msg: "Error in adding organization data.",
+                condition: false
+            }
+            res.send(output)
+        }
+    }
+   
 }
 
 orgController.getOrganizations = async(req, res) => {
@@ -29,8 +50,28 @@ orgController.getOrganizations = async(req, res) => {
 
 orgController.getOrganizationByName = async(req, res) => {``
     let orgData = await orgModel.findOne({ organizationName:req.query.name })
+    res.send(orgData);
+}
 
-    res.send(orgData)
+orgController.deleteOrganization = async(req, res) => {
+
+    if(req.query.id !== "undefined") {
+        try {
+            await orgModel.deleteOne({ _id : req.query.id })
+            var output = {
+                msg: "Organization data deleted successfully.",
+                condition: true
+            }
+            res.status(200).send(output)
+        }
+        catch(error) {
+            var output = {
+                msg: "Error in deleting organization data.",
+                condition: false
+            }
+            res.status(400).send(output)
+        }
+    }
 }
 
 module.exports = orgController;
--- controllers/timesheetController.js
+++ controllers/timesheetController.js
@@ -3,11 +3,11 @@ const moment = require('moment');
 
 const timeSheetController = {};
 
-timeSheetController.addTimesheet = (req, res) => {
+timeSheetController.addTimesheet = async (req, res) => {
 
     // console.log("body", req.body)
 
-    timesheetModel.findOne({ $and : [{ project : req.body.projectId.value, "userId.value" : req.body.userId._id }]}, (err, data) => {
+    timesheetModel.findOne({ $and : [{ project : req.body.projectId.value, "userId.value" : req.body.userId._id }]}, async (err, data) => {
         if(err) {
             res.status(500).send("Error");
         }
@@ -17,63 +17,72 @@ timeSheetController.addTimesheet = (req, res) => {
             let monthNo = moment().month() + 1;
             let yearNo = moment().year();
             let weekNo = req.body.weekNo;
-
             let array = [];
+            let oldEventsArray = [];
 
             for (var i = 0; i < weekData.length; i++) {
-                let obj = {
-                    start: weekData[i].date,
-                    end: weekData[i].date,
-                    title : weekData[i].hours,
-                    weekNo: weekNo,
-                    month : monthNo,
-                    year : yearNo,
-                    isAllDay: true,
-                    lock: false
+                // console.log(weekData[i])
+                if(weekData[i].title.length > 0) {
+                    let obj = {
+                        start: moment(new Date(weekData[i].date)).format("MM-DD-YYYY"),
+                        end: moment(new Date(weekData[i].date)).format("MM-DD-YYYY"),
+                        title : weekData[i].title,
+                        weekNo: weekNo,
+                        month : monthNo,
+                        year : yearNo,
+                        isAllDay: true,
+                        lock: false
+                    }
+    
+                    array.push(obj)
                 }
-                array.push(obj)
             }
+
             if (data.events.length > 0) {
-                //  console.log("array", data.events)
-                let oldEventsArray = [];
                 for (var j = 0; j < data.events.length; j++) {
-                    //  console.log(data.events[j].weekNo)
-                    if (data.events[j].weekNo === weekNo) {
-                        // console.log("haiii", weekNo)
-                        timesheetModel.updateOne({ "userId.value": req.body.userId._id }, { $set: { events: array } }, (error, update) => {
-                            if (error) {
-                                console.log(error)
-                            }
-                            else {
-                                console.log(update)
-                            }
-                        })
+                    oldEventsArray.push(data.events[j].start);
+                }
+                for (var k = 0; k < array.length; k++) {
+                    let newWeekDate = array[k].start
+                    if (oldEventsArray.includes(newWeekDate)) {
+
+                        if(array[k].title.length > 0){
+                        try {
+                            await timesheetModel.updateMany({ $and: [{ project: req.body.projectId.value, "userId.value": req.body.userId._id, "events.start": array[k].start }] }, { $set: { "events.$": array[k] } });
+                        }
+                        catch(error) {}
+                    }
                     }
                     else {
-                        // console.log("no week", weekNo)
-
-                        timesheetModel.updateOne({ "userId.value": req.body.userId._id }, { $push: { events: array } }, (error, update) => {
-                            if (error) {
-                                console.log(error)
+                        if(array[k].title.length > 0){
+                            try {
+                                await timesheetModel.updateMany({ $and: [{ project: req.body.projectId.value, "userId.value": req.body.userId._id }] }, { $push: { events: array[k] } });
                             }
-                            else {
-                                console.log(update)
-                            }
-                        })
+                            catch(error) {}
+                        }
                     }
                 }
-
-            }
-            else {
-                timesheetModel.updateOne({ "userId.value": req.body.userId._id }, { $push: { events: array } }, (error, update) => {
-                    if (error) {
-                        console.log(error)
-                    }
-                    else {
-                        console.log(update)
-                    }
+                res.send({
+                    msg: "Timesheet data updated successfully.",
+                    condition: true
                 })
 
+            }
+            
+            else {
+                try {
+                    await timesheetModel.updateOne({ "userId.value": req.body.userId._id }, { $push: { events: array } });
+                    res.send({
+                        msg: "Timesheet data added successfully.",
+                        condition: true
+                    })
+                }
+                catch(error){
+                    res.send({
+                        msg: "Timesheet data adding failed.",
+                        condition: false
+                    })
+                }
             }
 
         }
