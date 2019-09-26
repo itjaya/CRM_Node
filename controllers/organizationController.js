6a170204ae0604581648e29dff6d619cb195f43a
shiva <shiva@Asokaas-Mac-mini-5.local>
2019-09-26 18:46:45 +0530

    Update organizationController.js

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -1,28 +1,116 @@
 const orgModel = require('../models/organizationModel');
 let orgController = {}
 
+
 orgController.addOrganization = async (req, res) => {
 
-    let org = await orgModel.findOne({ organizationName : req.body.organizationName })
-    if(org == null) {
-        if(req.body._id) {
-            try {
-                await orgModel.updateOne({ _id : req.body._id }, { $set : req.body })
-                let output = {
-                    msg: "Organization data updated successfully.",
-                    condition: true
-                };
-                res.send(output)
+    if (req.body._id) {
+        orgModel.findOne({ "_id": req.body._id }, function (err, result) {
+
+            if (err) console.log(err)
+
+
+            else if (result) {
+
+                orgModel.findOne({ "_id": req.body._id }, function (err, result1) {
+
+                    if (err) console.log(err)
+
+                    else if (result1) {
+
+                        orgModel.updateOne({ '_id': req.body._id }, { $set: req.body }, function (err, result2) {
+
+                            if (!err) {
+                                if (result2.nModified == 0) {
+                                    var output = {
+                                        msg: "Organization data not modified.",
+                                        condition: true,
+                                    }
+                                    res.send(output)
+                                }
+
+                                else {
+                                    orgModel.findOne({ "_id": req.body._id }, function (err, result3) {
+                                        if (!err) {
+                                            var out = {
+                                                msg: "Organization data updated successfully.",
+                                                condition: true,
+                                            }
+                                            res.send(out);
+                                        }
+                                    });
+                                }
+                            }
+                            else {
+
+                                var out = {
+                                    msg: 'update failed like did not matched id.',
+                                    condition: false,
+                                }
+                                res.send(out);
+                            }
+                        });
+                    }
+                })
             }
-            catch (error) {
-                let output = {
-                    msg: "Error in updating organization data.",
-                    condition: false
-                }
-                res.send(output)
+            else {
+                orgModel.findOne({ "_id": req.body._id }, function (err, result4) {
+
+                    if (err) console.log(err)
+
+                    else {
+                        if (result4 == null) {
+                            orgModel.updateOne({ '_id': req.body._id }, { $set: req.body }, function (err, result5) {
+
+                                if (!err) {
+                                    if (result5.nModified == 0) {
+                                        var output = {
+                                            msg: "Organization data not modified",
+                                            condition: true,
+                                        }
+                                        res.send(output)
+
+                                    }
+                                    else {
+                                        orgModel.findOne({ "_id": req.body._id }, function (err, result6) {
+                                            if (!err) {
+                                                condition = true;
+                                                var out = {
+                                                    msg: "Organization data updated successfully.",
+                                                    condition: true,
+                                                }
+                                                res.send(out);
+                                            }
+                                        });
+                                    }
+                                } else {
+
+                                    var out = {
+                                        msg: 'update failed like did not matched id.',
+                                        condition: false,
+                                    }
+                                    res.send(out);
+
+                                }
+                            });
+                        }
+                        else {
+                            condition = false;
+                            var out = {
+                                msg: "Organization data exists.",
+                                condition: false,
+                            }
+                            res.send(out);
+                        }
+                    }
+                })
             }
-        }
-        else {
+        })
+    }
+    else {
+        let org = await orgModel.findOne({ organizationName: req.body.organizationName })
+        if (org !== null) {
+
             try {
                 let orgData = new orgModel(req.body);
                 await orgData.save();
@@ -40,15 +128,18 @@ orgController.addOrganization = async (req, res) => {
                 res.send(output)
             }
         }
-    }
-    else {
-        let output = {
-            msg: "Organization name already exists.",
-            condition: false
+        else {
+            let output = {
+                msg: "Organization name exists.",
+                condition: false
+            }
+            res.send(output)
         }
-        res.send(output)
+
     }
+
 }
+
 
 orgController.getOrganizations = async(req, res) => {
 
